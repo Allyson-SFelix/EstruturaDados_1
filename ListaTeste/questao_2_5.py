52906f56e2d419a4dbd4a39df6dcb65af987abee
Allyson da Silva Felix <98405884+Allyson-SFelix@users.noreply.github.com>
2023-11-30 00:19:40 +0000

    Questões 2 e 5

---
+++ ListaTeste/questao_2_5.py
@@ -0,0 +1,3 @@
+
+print("Na questão 5, irá ocorrer a saida do numero 2 e depois ocorrerá a saida do 200. \n")
+print("Na questão 2, irá ocorrer a saida do numero (-5), pois [(0)+(1*-1*4)+(0)-(1*1*1)-(0)-(0)]=(-5).")
